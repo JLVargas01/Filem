a87a0e9d20b4c572f4e0d396426ace9324564a50
JLVargas01 <vargasibarra01@gmail.com>
2025-05-13 17:13:07 -0600

    Manejador de archivos

---
+++ app/src/main/java/com/spiralsoft/filem/PermissionUtils.kt
@@ -0,0 +1,43 @@
+package com.spiralsoft.filem
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Environment
+import android.provider.Settings
+import androidx.activity.result.ActivityResultLauncher
+import android.content.pm.PackageManager
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
+
+object PermissionUtils {
+
+    fun hasPermissions(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Environment.isExternalStorageManager()
+        } else {
+            ContextCompat.checkSelfPermission(
+                context, Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    fun requestPermissionsPreR(launcher: ActivityResultLauncher<Array<String>>) {
+        launcher.launch(
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    fun requestPermissions(activity: Activity) {
+        val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+        intent.data = ("package:" + activity.packageName).toUri()
+        activity.startActivity(intent)
+    }
+}
