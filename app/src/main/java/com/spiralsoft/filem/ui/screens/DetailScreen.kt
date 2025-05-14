898589b893ee6a908a60670a36d562276d997793
JLVargas01 <vargasibarra01@gmail.com>
2025-05-14 12:04:44 -0600

    Pantalla de muestra

---
+++ app/src/main/java/com/spiralsoft/filem/ui/screens/DetailScreen.kt
@@ -0,0 +1,9 @@
+package com.spiralsoft.filem.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailScreen(name: String?) {
+    Text("Hola $name")
+}
