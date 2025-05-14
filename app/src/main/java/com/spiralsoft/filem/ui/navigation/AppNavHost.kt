8908f91991d4e833aac8eb23a0c1dbfcdd9f866d
JLVargas01 <vargasibarra01@gmail.com>
2025-05-14 10:54:50 -0600

    Archivo para navegacion

---
+++ app/src/main/java/com/spiralsoft/filem/ui/navigation/AppNavHost.kt
@@ -0,0 +1,28 @@
+/*
+** Define rutas, pantallas y controladores
+*/
+
+package com.spiralsoft.filem.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.spiralsoft.filem.ui.screens.DetailScreen
+import com.spiralsoft.filem.ui.screens.HomeScreen
+
+@Composable
+fun AppNavHost() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen(onNavigateToDetails = {
+                navController.navigate("details")
+            })
+        }
+        composable("details") {
+            DetailScreen()
+        }
+    }
+}
