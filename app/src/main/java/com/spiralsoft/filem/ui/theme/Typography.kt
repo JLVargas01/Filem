097f7527758fcae2df21ce51f44d01dc7ead20e4
JLVargas01 <vargasibarra01@gmail.com>
2025-05-14 12:08:05 -0600

    Archivo de definicion de tipografias

---
+++ app/src/main/java/com/spiralsoft/filem/ui/theme/Typography.kt
@@ -0,0 +1,19 @@
+package com.spiralsoft.filem.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
+
+val AppTypography = Typography(
+    bodyLarge = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontFamily = FontFamily.SansSerif
+    ),
+    titleLarge = TextStyle(
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        fontFamily = FontFamily.SansSerif
+    )
+)
