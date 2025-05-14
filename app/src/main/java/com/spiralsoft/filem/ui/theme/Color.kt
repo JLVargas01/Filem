d5f1105e6f167d89a85b2e9fdfb804991d693761
JLVargas01 <vargasibarra01@gmail.com>
2025-05-14 10:46:47 -0600

    Colores de ejemplo

--- app/src/main/java/com/spiralsoft/filem/ui/theme/Color.kt
+++ app/src/main/java/com/spiralsoft/filem/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.spiralsoft.filem.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary = Color(0xFF0061A4)
+val OnPrimary = Color(0xFFFFFFFF)
+val Background = Color(0xFFF0F0F0)
+val Surface = Color(0xFFFFFFFF)
+val OnSurface = Color(0xFF1B1B1F)
+val Secondary = Color(0xFF005A5F)
+val Error = Color(0xFFB00020)
