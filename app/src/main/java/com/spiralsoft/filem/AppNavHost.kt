e38b3e394a2dc0a17778fd9dfba04ec03aba2183
JLVargas01 <vargasibarra01@gmail.com>
2025-05-15 12:50:38 -0600

    Mod: Rutas de navegacion iniciales

--- app/src/main/java/com/spiralsoft/filem/AppNavHost.kt
+++ app/src/main/java/com/spiralsoft/filem/AppNavHost.kt
@@ -1,24 +1,17 @@
 package com.spiralsoft.filem
 
-import android.net.Uri
-import android.os.Environment
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.spiralsoft.filem.ui.screens.FileExplorerScreen
-import com.spiralsoft.filem.ui.screens.SettingsScreen
-import com.spiralsoft.filem.viewmodel.ThemeViewModel
 
 object Routes {
     const val EXPLORER = "explorer"
-    const val SETTINGS = "settings"
 }
 
 @Composable
-fun AppNavHost(themeViewModel: ThemeViewModel) {
+fun AppNavHost() {
 
     val navController = rememberNavController()
 
@@ -28,16 +21,9 @@ fun AppNavHost(themeViewModel: ThemeViewModel) {
     ) {
         composable(Routes.EXPLORER) {
             FileExplorerScreen(
-                onNavigateTo = {},
-                onNavigateToSettings = {
-                    navController.navigate(Routes.SETTINGS)
-                }
+                onNavigateTo = {}
             )
         }
-
-        composable(Routes.SETTINGS) {
-            SettingsScreen(navController, themeViewModel)
-        }
-
     }
+
 }
