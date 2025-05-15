df1bdffc2f43c8cf7a85c8b7a60b22e5f977f031
JLVargas01 <vargasibarra01@gmail.com>
2025-05-14 22:41:27 -0600

    Mod: Pantalla de ajustes

--- app/src/main/java/com/spiralsoft/filem/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/spiralsoft/filem/ui/screens/SettingsScreen.kt
@@ -1,21 +1,59 @@
 package com.spiralsoft.filem.ui.screens
 
+import android.os.Environment
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.spiralsoft.filem.ui.theme.ThemeViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingsScreen(onBack: () -> Unit) {
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-        Text("Ajustes", style = MaterialTheme.typography.titleLarge)
+fun SettingsScreen(
+    navController: NavController,
+    themeViewModel: ThemeViewModel
+) {
+    val isDarkTheme by themeViewModel.isDarkTheme.collectAsState()
 
-        Button(
-            onClick = onBack,
-            modifier = Modifier.padding(top = 24.dp)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Ajustes") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Volver")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp)
         ) {
-            Text("Volver")
+            Text(
+                text = "Preferencias",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text("Modo Oscuro")
+                Switch(
+                    checked = isDarkTheme,
+                    onCheckedChange = { themeViewModel.toggleTheme() }
+                )
+            }
         }
     }
 }
