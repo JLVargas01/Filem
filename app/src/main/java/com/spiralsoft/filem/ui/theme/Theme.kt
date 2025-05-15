79e85e53c96ea47714d91872553eab08499ecd2b
JLVargas01 <vargasibarra01@gmail.com>
2025-05-14 22:42:23 -0600

    Add: Valores para tema dark  light

--- app/src/main/java/com/spiralsoft/filem/ui/theme/Theme.kt
+++ app/src/main/java/com/spiralsoft/filem/ui/theme/Theme.kt
@@ -2,21 +2,39 @@ package com.spiralsoft.filem.ui.theme
 
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val LightColors = lightColorScheme(
-    primary = Primary,
-    onPrimary = OnPrimary,
-    secondary = Secondary,
-    background = Background,
-    surface = Surface,
-    onSurface = OnSurface,
-    error = Error
+    primary = LightBlue40,
+    onPrimary = Color.White,
+    secondary = LightDarkBlue,
+    background = LightGray,
+    surface = Color.White,
+    error = ErrorRed,
+    onBackground = Color.Black,
+    onSurface = Color.Black
+)
+
+private val DarkColors = darkColorScheme(
+    primary = DarkPrimary,
+    onPrimary = DarkOnPrimary,
+    secondary = LightBlue80,
+    background = DarkBackground,
+    surface = DarkSurface,
+    error = ErrorRed,
+    onBackground = DarkOnBackground,
+    onSurface = DarkOnBackground
 )
 
 @Composable
-fun FilemTheme(content: @Composable () -> Unit) {
+fun FilemTheme(
+    useDarkTheme: Boolean,
+    content: @Composable () -> Unit
+) {
+    val colors = if (useDarkTheme) DarkColors else LightColors
+
     MaterialTheme(
-        colorScheme = LightColors,
+        colorScheme = colors,
         typography = AppTypography,
         shapes = AppShapes,
         content = content
