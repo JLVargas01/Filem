ac13443b2567d6002d7d46d3b25ec87773619510
JLVargas01 <vargasibarra01@gmail.com>
2025-05-14 21:53:18 -0600

    Add: Pantalla inicial de pantalla home

--- app/src/main/java/com/spiralsoft/filem/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/spiralsoft/filem/ui/screens/HomeScreen.kt
@@ -3,13 +3,17 @@ package com.spiralsoft.filem.ui.screens
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Column
+
 
 @Composable
 fun HomeScreen(onNavigateToDetails: () -> Unit) {
+    Column {
         Text("Pantalla Principal")
         Button(onClick = onNavigateToDetails) {
             Text("Ir a Detalles")
         }
+    }
 }
 
 @Composable
