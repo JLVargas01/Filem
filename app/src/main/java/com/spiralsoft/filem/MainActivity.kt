91711389a6ed1a0690d7179f2b4200c6bbf98268
JLVargas01 <vargasibarra01@gmail.com>
2025-05-15 12:51:54 -0600

    MainActivity: Manejo de permisos de almacenamiento

--- app/src/main/java/com/spiralsoft/filem/MainActivity.kt
+++ app/src/main/java/com/spiralsoft/filem/MainActivity.kt
@@ -5,48 +5,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import com.spiralsoft.filem.ui.AppNavHost
-import com.spiralsoft.filem.ui.theme.FilemTheme
-import com.spiralsoft.filem.ui.theme.ThemeViewModel
-import com.spiralsoft.filem.ui.theme.ThemeViewModelFactory
+import com.spiralsoft.filem.utils.PermissionUtils
+import android.widget.Toast
 
 class MainActivity : ComponentActivity() {
 
-    // Lanzador moderno de permisos
-    private val permissionLauncher = registerForActivityResult(
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestStoragePermissions()
+        setContent {
+            AppNavHost()
+        }
+    }
+
+    private val storagePermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
     ) { permissions ->
         val allGranted = permissions.values.all { it }
         if (!allGranted) {
-            // Aquí puedes mostrar un mensaje o cerrar la app si lo deseas
+            Toast.makeText(this, "Se requieren permisos para usar la app", Toast.LENGTH_LONG).show()
+            finish()
         }
     }
 
-    private val themeViewModel: ThemeViewModel by viewModels {
-        ThemeViewModelFactory(applicationContext)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            val isDarkTheme by themeViewModel.isDarkTheme.collectAsState()
-            FilemTheme(useDarkTheme = isDarkTheme) {
-                AppNavHost(themeViewModel)
-            }
-        }
-
-        // Pedimos permisos si no están concedidos
+    private fun requestStoragePermissions() {
         if (!PermissionUtils.hasPermissions(this)) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                PermissionUtils.requestPermissions(this)
+                PermissionUtils.requestAllFilesAccessPermission(this)
             } else {
-                PermissionUtils.requestPermissionsPreR(permissionLauncher)
+                PermissionUtils.requestLegacyStoragePermissions(storagePermissionLauncher)
             }
         }
-
     }
 
 }
