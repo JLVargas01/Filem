06aae56648bb8534505f75b14a3eb2bbb9735d9c
JLVargas01 <vargasibarra01@gmail.com>
2025-05-18 13:34:37 -0600

    Add: agregó la propiedad isLoading a la clase FileExplorerState para indicar si se están cargando archivos.

--- app/src/main/java/com/spiralsoft/filem/viewmodel/FileExplorerState.kt
+++ app/src/main/java/com/spiralsoft/filem/viewmodel/FileExplorerState.kt
@@ -1,10 +1,12 @@
 package com.spiralsoft.filem.viewmodel
 
+import android.os.Environment
 import java.io.File
 
 data class FileExplorerState(
-    val currentPath: String = android.os.Environment.getExternalStorageDirectory().absolutePath,
+    val currentPath: String = Environment.getExternalStorageDirectory().absolutePath,
     val files: List<File> = emptyList(),
     val sortMode: SortMode = SortMode.BY_NAME,
+    val isLoading: Boolean = false,
     val showHiddenFiles: Boolean = false, // Aún no implementado
 )
