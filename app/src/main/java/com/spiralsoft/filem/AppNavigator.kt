69f2f415c3abaed1147e060ec89a975348535738
JLVargas01 <vargasibarra01@gmail.com>
2025-05-18 13:33:43 -0600

    Add: Agregando navegador entre pantallas

---
+++ app/src/main/java/com/spiralsoft/filem/AppNavigator.kt
@@ -0,0 +1,33 @@
+package com.spiralsoft.filem
+
+import android.net.Uri
+import android.os.Environment
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+
+object AppNavigator {
+
+    object Hub {
+        const val ROUTE = "hub"
+    }
+
+    object Explorer {
+        private const val BASE = "explorer"
+        const val ROUTE = "$BASE/{path}"
+
+        fun createRoute(path: String): String {
+            return "$BASE/${Uri.encode(path)}"
+        }
+
+        fun navArgument(): NamedNavArgument {
+            return navArgument("path") { type = NavType.StringType }
+        }
+
+        fun extractPath(backStackEntry: NavBackStackEntry): String {
+            return backStackEntry.arguments?.getString("path")?.let { Uri.decode(it) }
+                ?: Environment.getExternalStorageDirectory().absolutePath
+        }
+    }
+}
