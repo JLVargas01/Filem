b32ceb28be76d382ae316de74c19fca7585ea8e9
JLVargas01 <vargasibarra01@gmail.com>
2025-05-19 13:00:31 -0600

    Add: Composable para el contenido del navegador de archivos

---
+++ app/src/main/java/com/spiralsoft/filem/ui/screens/NavContent.kt
@@ -0,0 +1,55 @@
+package com.spiralsoft.filem.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.Alignment
+import com.spiralsoft.filem.ui.components.DirectoryItem
+import com.spiralsoft.filem.viewmodel.FileExplorerState
+import com.spiralsoft.filem.viewmodel.HubFileExplorerState
+
+@Composable
+fun NavContent(
+    state: FileExplorerState, // Estado de la pantalla
+    modifier: Modifier = Modifier, // Modificador para personalizar la apariencia
+    onNavigateTo: (String) -> Unit // Función para navegar a una ruta
+) {
+    when {
+
+        // Mostrar el indicador de carga
+        state.isLoading -> {
+            Box(modifier = modifier, contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
+
+        // Mostrar el contenido de la lista de directorios
+        state.directories.isEmpty() -> {
+            Box(modifier = modifier, contentAlignment = Alignment.Center) {
+                Text("No se encontraron directorios disponibles.")
+            }
+        }
+
+        // Mostrar el contenido de la lista de directorios
+        else -> {
+            LazyColumn(
+                modifier = modifier,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(state.directories) { dir ->
+                    DirectoryItem(
+                        dir = dir,
+                        onClick = { onNavigateTo(dir.absolutePath) }
+                    )
+                }
+            }
+        }
+
+    }
+}
