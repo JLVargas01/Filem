e6d69a26c34a60af20c506dd1619727449787ea4
JLVargas01 <vargasibarra01@gmail.com>
2025-05-19 14:04:09 -0600

    Add: lista de archivos en estado hub

--- app/src/main/java/com/spiralsoft/filem/viewmodel/HubFileExplorerState.kt
+++ app/src/main/java/com/spiralsoft/filem/viewmodel/HubFileExplorerState.kt
@@ -3,6 +3,7 @@ package com.spiralsoft.filem.viewmodel
 import java.io.File
 
 data class HubFileExplorerState(
-    val directories: List<File> = emptyList(),
-    val isLoading: Boolean = false,
+    val files: List<File> = emptyList(), // Lista de archivos disponibles
+    val directories: List<File> = emptyList(), // Lista de directorios disponibles
+    val isLoading: Boolean = false, //Estado de carga
 )
