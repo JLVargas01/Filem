d5972dec99246f414f74fe05a05658ba914e9dd4
JLVargas01 <vargasibarra01@gmail.com>
2025-05-19 14:05:07 -0600

    Add: Funcion para cargar archivos en el rootPath

--- app/src/main/java/com/spiralsoft/filem/viewmodel/HubFileExplorerViewModel.kt
+++ app/src/main/java/com/spiralsoft/filem/viewmodel/HubFileExplorerViewModel.kt
@@ -11,20 +11,24 @@ import android.os.Environment
 
 class HubFileExplorerViewModel : ViewModel() {
 
-    private val _state = MutableStateFlow(HubFileExplorerState())
-    val state: StateFlow<HubFileExplorerState> get() = _state
+    private val _state = MutableStateFlow(HubFileExplorerState()) // Estado de la pantalla privado
+    val state: StateFlow<HubFileExplorerState> get() = _state  // Estado de la pantalla público
 
+    // Cargar directorios al iniciar la pantalla
     init {
         loadRootDirectories()
+        loadRootFiles()
     }
 
+    // Cargar directorios en root
     fun loadRootDirectories() {
         viewModelScope.launch(Dispatchers.IO) {
-            _state.value = _state.value.copy(isLoading = true)
+            _state.value = _state.value.copy(isLoading = false)
 
             val roots = mutableListOf<File>()
 
             val internalStorage = Environment.getExternalStorageDirectory()
+
             if (internalStorage.exists() && internalStorage.canRead()) {
                 val subDirs = internalStorage.listFiles()?.filter {
                     it.isDirectory && it.canRead() && !it.isHidden
@@ -39,6 +43,35 @@ class HubFileExplorerViewModel : ViewModel() {
 
             _state.value = _state.value.copy(
                 directories = roots,
+                isLoading = false
+            )
+        }
+    }
+
+    // Cargar archivos en root
+    fun loadRootFiles() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _state.value = _state.value.copy(isLoading = false)
+
+            val roots = mutableListOf<File>()
+
+            val internalStorage = Environment.getExternalStorageDirectory()
+
+            if (internalStorage.exists() && internalStorage.canRead()) {
+                val subFiles = internalStorage.listFiles()?.filter {
+                    it.exists() && it.canRead() && it.isFile && it != internalStorage
+                }.orEmpty()
+                roots.addAll(subFiles)
+            }
+
+            val externalDirs = File("/storage").listFiles()?.filter {
+                it.exists() && it.canRead() && it.isFile && it != internalStorage
+            }.orEmpty()
+
+            roots.addAll(externalDirs)
+
+            _state.value = _state.value.copy(
+                files = roots,
                 isLoading = false
             )
         }
