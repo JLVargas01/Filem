7121085ddaac9bd3413f377e54970967cb4c985a
JLVargas01 <vargasibarra01@gmail.com>
2025-05-20 17:16:56 -0600

    Mod: Renombramiento archivo

--- app/src/main/java/com/spiralsoft/filem/viewmodel/HubDirectoryExplorerState.kt
+++ app/src/main/java/com/spiralsoft/filem/viewmodel/HubDirectoryExplorerState.kt
@@ -2,7 +2,7 @@ package com.spiralsoft.filem.viewmodel
 
 import java.io.File
 
-data class HubFileExplorerState(
+data class HubDirectoryExplorerState(
     val files: List<File> = emptyList(), // Lista de archivos disponibles
     val directories: List<File> = emptyList(), // Lista de directorios disponibles
     val isLoading: Boolean = false, //Estado de carga
