c77bb9e51ae1e3b55fbe2c7b12c44cdaeda5e91b
JLVargas01 <vargasibarra01@gmail.com>
2025-05-22 15:34:31 -0600

    Add: Nueva dependencia

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material3)
+    implementation(libs.coil.compose)
     implementation(platform(libs.androidx.compose.bom))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
