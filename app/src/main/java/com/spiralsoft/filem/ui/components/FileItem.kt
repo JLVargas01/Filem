7e9c660ae8898dbdd22edd8314d642df98b7f011
JLVargas01 <vargasibarra01@gmail.com>
2025-05-21 22:34:55 -0600

    Add: Apertura de archivos al hacer click e icono para identificar

--- app/src/main/java/com/spiralsoft/filem/ui/components/FileItem.kt
+++ app/src/main/java/com/spiralsoft/filem/ui/components/FileItem.kt
@@ -3,33 +3,44 @@ package com.spiralsoft.filem.ui.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.width
-import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Alignment
-import com.spiralsoft.filem.R
+import androidx.compose.ui.platform.LocalContext
 import com.spiralsoft.filem.utils.cleanPath
+import com.spiralsoft.filem.constants.FileType
+import com.spiralsoft.filem.utils.FileOpener
 import java.io.File
 
 @Composable
 fun FileItem(
     dir: File,
-    onClick: () -> Unit
+    modifier: Modifier = Modifier
 ) {
+
+    val context = LocalContext.current
+    val fileType = remember(dir) { FileType.fromFile(dir) }
+
     Card(
-        modifier = Modifier.fillMaxWidth().clickable { /*onClick() No hace nada si se clica*/ },
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable {
+                FileOpener.openFile(context, dir)
+            },
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
         shape = RoundedCornerShape(12.dp)
     ) {
@@ -38,8 +49,8 @@ fun FileItem(
             verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
-                painter = painterResource(id = R.drawable.icon_file0),
-                contentDescription = "Ícono de carpeta",
+                painter = painterResource(id = fileType.iconRes),
+                contentDescription = "Ícono de archivo",
                 modifier = Modifier.size(24.dp)
             )
 
