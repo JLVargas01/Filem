aeb7db9e72637301d4c7e77a2b108b99b11e6321
JLVargas01 <vargasibarra01@gmail.com>
2025-05-23 23:27:58 -0600

    Add: Validacion de app de destino

--- app/src/main/java/com/spiralsoft/filem/constants/FileType.kt
+++ app/src/main/java/com/spiralsoft/filem/constants/FileType.kt
@@ -1,8 +1,9 @@
 package com.spiralsoft.filem.constants
 
 import android.content.Context
-import com.spiralsoft.filem.R
+import android.widget.Toast
 import java.io.File
+import com.spiralsoft.filem.R
 import com.spiralsoft.filem.utils.FileOpener
 
 enum class FileType(
@@ -11,72 +12,90 @@ enum class FileType(
     val mimeType: String,
     val onClickAction: ((Context, File) -> Unit)
 ) {
-
-
     IMAGE(
         iconRes = R.drawable.icon_img0,
         mimeType = "image/*",
         onClickAction = { context, file ->
-            FileOpener.openImage(context, file)
+            if (FileOpener.canOpenFile(context, file, "video/*")) {
+                FileOpener.openVideo(context, file)
+            } else {
+                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
+            }
         }
     ),
-
     AUDIO(
         iconRes = R.drawable.icon_audio0,
         mimeType = "audio/*",
         onClickAction = { context, file ->
-            FileOpener.openAudio(context, file)
+            if (FileOpener.canOpenFile(context, file, "video/*")) {
+                FileOpener.openAudio(context, file)
+            } else {
+                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
+            }
         }
     ),
-
     VIDEO(
         iconRes = R.drawable.icon_video0,
         mimeType = "video/*",
         onClickAction = { context, file ->
-            FileOpener.openVideo(context, file)
+            if (FileOpener.canOpenFile(context, file, "video/*")) {
+                FileOpener.openVideo(context, file)
+            } else {
+                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
+            }
         }
     ),
-
     DOCUMENT(
         iconRes = R.drawable.icon_document0,
         mimeType = "text/*",
         onClickAction = { context, file ->
-            FileOpener.openText(context, file)
+            if (FileOpener.canOpenFile(context, file, "video/*")) {
+                FileOpener.openText(context, file)
+            } else {
+                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
+            }
         }
     ),
-
     PDF(
         iconRes = R.drawable.icon_pdf0,
         mimeType = "application/pdf",
         onClickAction = { context, file ->
-            FileOpener.openPdf(context, file)
+            if (FileOpener.canOpenFile(context, file, "video/*")) {
+                FileOpener.openPdf(context, file)
+            } else {
+                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
+            }
         }
     ),
-
     ARCHIVE(
         iconRes = R.drawable.icon_archive0,
         mimeType = "application/zip",
         onClickAction = { context, file ->
-            FileOpener.openGeneric(context, file)
+            if (FileOpener.canOpenFile(context, file, "video/*")) {
+                FileOpener.openCompressed(context, file)
+            } else {
+                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
+            }
         }
     ),
-
     DATAFILE(
         iconRes = R.drawable.icon_datafile0,
         mimeType = "application/octet-stream",
         onClickAction = { context, file ->
-            FileOpener.openGeneric(context, file)
+            if (FileOpener.canOpenFile(context, file, "video/*")) {
+                FileOpener.openText(context, file)
+            } else {
+                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
+            }
         }
     ),
-
     UNKNOWN(
         iconRes = R.drawable.icon_unkdown,
-        mimeType = "*/*",
-        onClickAction = { context, file ->
-            FileOpener.openGeneric(context, file)
+        mimeType = "",
+        onClickAction = { context, _ ->
+            Toast.makeText(context, "No se puede abrir este tipo de archivo", Toast.LENGTH_SHORT).show()
         }
     );
-
     companion object {
         fun fromFile(file: File): FileType {
             val extension = file.extension.lowercase()
@@ -93,3 +112,14 @@ enum class FileType(
         }
     }
 }
+
+/* NOTA: Implementar un codigo como este en el futuro para evitar tanta repeticion
+Esque me dio weba
+fun doOpenFile(context: Context, file: File, type: String) {
+    if (FileOpener.canOpenFile(context, file, type.mimeType)) {
+        type.onClickAction(context, file)
+    } else {
+        Toast.makeText(context, "No hay apps para abrir este archivo", Toast.LENGTH_SHORT).show()
+    }
+}
+*/
