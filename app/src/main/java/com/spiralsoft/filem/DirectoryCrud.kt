7bb3929cf8c6633ac99340bf8a4c0683fbd7c3ec
JLVargas01 <vargasibarra01@gmail.com>
2025-05-30 20:55:28 -0600

    Add:DirectoryCrud interface.

---
+++ app/src/main/java/com/spiralsoft/filem/DirectoryCrud.kt
@@ -0,0 +1,14 @@
+package com.spiralsoft.filem
+
+import java.io.File
+
+interface DirectoryCrud {
+
+    fun createDirectory(name: String): Boolean // Create
+    fun getDirectories(directoryPath: File): List<File> // Read
+    fun renameDirectory(dir: File, newName: String): Boolean // Update
+    fun moveDirectory(file: File, targetDir: File): Boolean // Update
+    fun copyDirectory(file: File, targetDir: File): Boolean // Update
+    fun deleteDirectory(dir: File): Boolean // Delete
+
+}
