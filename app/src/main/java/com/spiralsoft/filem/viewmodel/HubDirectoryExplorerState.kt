c0d77818bf6b806d0b465ae2016546347fa08608
JLVargas01 <vargasibarra01@gmail.com>
2025-05-30 21:27:06 -0600

    Add: nuevo estado selectedItems

--- app/src/main/java/com/spiralsoft/filem/viewmodel/HubDirectoryExplorerState.kt
+++ app/src/main/java/com/spiralsoft/filem/viewmodel/HubDirectoryExplorerState.kt
@@ -9,4 +9,5 @@ data class HubDirectoryExplorerState(
     val files: List<File> = emptyList(), // Lista de archivos disponibles
     val directories: List<File> = emptyList(), // Lista de directorios disponibles
     val isLoading: Boolean = false, //Estado de carga
+    val selectedItems: Set<File> = emptySet() // Lista de elementos seleccionados
 )
