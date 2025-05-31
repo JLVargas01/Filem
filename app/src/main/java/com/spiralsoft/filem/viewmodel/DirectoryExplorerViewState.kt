52956cddd7b47f789f20303599fe047d91845928
JLVargas01 <vargasibarra01@gmail.com>
2025-05-30 21:20:18 -0600

    add: Introduce estado para selección múltiple en DirectoryExplorerViewState

--- app/src/main/java/com/spiralsoft/filem/viewmodel/DirectoryExplorerViewState.kt
+++ app/src/main/java/com/spiralsoft/filem/viewmodel/DirectoryExplorerViewState.kt
@@ -9,5 +9,6 @@ data class DirectoryExplorerViewState(
     val files: List<File> = emptyList(), // Lista de archivos disponibles
     val directories: List<File> = emptyList(), // Lista de directorios disponibles
     val isLoading: Boolean = false, // Estado de carga
+    val selectedItems: Set<File> = emptySet(), // Lista de elementos seleccionados
     val currentPath: String // Ruta actual
 )
