cd4c36530521766055565627b25de7202774cd1e
JLVargas01 <vargasibarra01@gmail.com>
2025-06-03 17:22:33 -0600

    mod: Cambio de variable state por estados genericos

--- app/src/main/java/com/spiralsoft/filem/presentation/components/DirectoryContent.kt
+++ app/src/main/java/com/spiralsoft/filem/presentation/components/DirectoryContent.kt
@@ -1,27 +1,26 @@
 /**
- * Representa el listado de los directorios y archivos,
- * solo se utiliza para mostrar y listar el contenido que no esta en el rootPath
+ * Contenido de la pantalla
+ * Enlista y muestra el contenido de un directorio
  */
-package com.spiralsoft.filem.presentation.screen.directory
+package com.spiralsoft.filem.presentation.components
 
+import com.spiralsoft.filem.domain.repository.DirectoryViewState
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Box
 import androidx.compose.ui.Alignment
-import com.spiralsoft.filem.presentation.components.FileListContent
 import java.io.File
 
 @Composable
 fun DirectoryContent(
-    state: DirectoryExplorerViewState, // Estado de la pantalla
+    state: DirectoryViewState, // Estado de la pantalla
     modifier: Modifier = Modifier, // Modificador para personalizar la apariencia
     onNavigateTo: (String) -> Unit, // Función para navegar a una ruta
     toggleSelection: (File) -> Unit // Función para seleccionar un elemento
 ) {
     when {
-
         // Mostrar el indicador de carga
         state.isLoading -> {
             Box(modifier = modifier, contentAlignment = Alignment.Center) {
@@ -35,10 +34,10 @@ fun DirectoryContent(
                 Text("Vacio")
             }
         }
-
         // Mostrar el contenido de la lista de directorios
         else -> {
             FileListContent(
+                modifier = modifier,
                 directories = state.directories,
                 files = state.files,
                 selectedItems = state.selectedItems,
@@ -46,6 +45,6 @@ fun DirectoryContent(
                 toggleSelection = toggleSelection
             )
         }
-
     }
+
 }
