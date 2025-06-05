4f2a1435a909f6a55a490d8511dfcbb2dea4a25f
JLVargas01 <vargasibarra01@gmail.com>
2025-06-05 17:54:17 -0600

    Refactor: Migracion de `java.io.File` a `java.nio.file.Path`

--- app/src/main/java/com/spiralsoft/filem/presentation/screen/hub/HubDirectoryExplorerState.kt
+++ app/src/main/java/com/spiralsoft/filem/presentation/screen/hub/HubDirectoryExplorerState.kt
@@ -4,11 +4,11 @@
 package com.spiralsoft.filem.presentation.screen.hub
 
 import com.spiralsoft.filem.domain.repository.DirectoryViewState
-import java.io.File
+import java.nio.file.Path
 
 data class HubDirectoryExplorerState(
-    override val files: List<File> = emptyList(), // Lista de archivos disponibles
-    override val directories: List<File> = emptyList(), // Lista de directorios disponibles
+    override val files: List<Path> = emptyList(), // Lista de archivos disponibles
+    override val directories: List<Path> = emptyList(), // Lista de directorios disponibles
     override val isLoading: Boolean = false, //Estado de carga
-    override val selectedItems: Set<File> = emptySet() // Lista de elementos seleccionados
+    override val selectedItems: Set<Path> = emptySet() // Lista de elementos seleccionados
 ) : DirectoryViewState
