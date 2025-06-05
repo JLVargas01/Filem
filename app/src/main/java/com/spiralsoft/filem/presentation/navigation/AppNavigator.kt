f1b5190c69d340447d0b38c76227897d06dc52ab
JLVargas01 <vargasibarra01@gmail.com>
2025-06-05 15:40:59 -0600

    mod: Cambios para obtener, devolver y manejar Paths

--- app/src/main/java/com/spiralsoft/filem/presentation/navigation/AppNavigator.kt
+++ app/src/main/java/com/spiralsoft/filem/presentation/navigation/AppNavigator.kt
@@ -1,3 +1,6 @@
+/**
+ * Ayuda para controlar y formar rutas de navegación
+ */
 package com.spiralsoft.filem.presentation.navigation
 
 import android.net.Uri
@@ -6,28 +9,38 @@ import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
+import java.nio.file.Path
+import java.nio.file.Paths
 
 object AppNavigator {
 
     object Hub {
-        const val ROUTE = "hub"
+        const val ROUTE: String = "hub"
     }
 
+    // Objeto para controlar la navegación del explorador de archivos
     object Explorer {
-        private const val BASE = "explorer"
-        const val ROUTE = "$BASE/{path}"
 
-        fun createRoute(path: String): String {
-            return "$BASE/${Uri.encode(path)}"
+        private const val BASE: String = "explorer"
+        const val ROUTE: String = "$BASE/{path}"
+
+        // Construye una ruta de navegación para el explorador de archivos
+        fun createRoute(path: Path): Path {
+            val route = "$BASE/${Uri.encode(path.toString())}"
+            return Paths.get(route)
         }
 
+        // Obtiene los argumentos de navegación para el explorador de archivos
         fun navArguments(): List<NamedNavArgument> {
             return listOf(navArgument("path") { type = NavType.StringType })
         }
 
-        fun extractPath(backStackEntry: NavBackStackEntry): String {
-            return backStackEntry.arguments?.getString("path")?.let { Uri.decode(it) }
+        // Extrae la ruta de navegación del explorador de archivos
+        fun extractPath(backStackEntry: NavBackStackEntry): Path {
+            val pathString = backStackEntry.arguments?.getString("path")
+                ?.let { Uri.decode(it) }
                 ?: Environment.getExternalStorageDirectory().absolutePath
+            return Paths.get(pathString)
         }
 
     }
